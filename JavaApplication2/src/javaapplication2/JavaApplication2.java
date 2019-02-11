787553087306e36e1be8e8cecdb0d2e97f0d6aa3
Hoang Thong <hoangthong3401@gmail.com>
2019-02-10 23:08:48 -0800

    first commit

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,13 @@
+package javaapplication2;
+
+/**
+ *
+ * @author Hoang
+ */
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first program with Github.");
+    }
+
+}
