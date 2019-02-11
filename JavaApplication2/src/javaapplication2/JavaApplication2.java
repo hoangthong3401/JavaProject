a984a4f130db42ed8999b52b04dbc7f0d57b9128
Hoang Thong <hoangthong3401@gmail.com>
2019-02-10 23:13:18 -0800

    chinh sua file main.class

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -8,6 +8,7 @@ public class JavaApplication2 {
 
     public static void main(String[] args) {
         System.out.println("This is my first program with Github.");
+        System.out.println("Hello guys!");
     }
 
 }
