cac4c5c0efac97447676b36d959683b69558c5d5
hoangthong3401 <hoangthong3401@gmail.com>
2019-02-19 21:41:30 +0700

    Testing ADD and SUB function

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -6,18 +6,6 @@ public class Main {
     public static void main(String[] args) {
         BigNumber.Tool obj = new BigNumber().new Tool();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a, b, check: ");
-        String a = obj.randomNumber(32, true);
-        String b = obj.randomNumber(32, true);
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println(obj.compare(a, b));
-        String check = sc.nextLine();
-        if (new BigNumber().sub(a, b).equals(check)) {
-            System.out.println("TRUE");
-        } else {
-            System.out.println("FALSE");
-        }
-        System.out.println(new BigNumber().sub(a, b));
+
     }
 }
